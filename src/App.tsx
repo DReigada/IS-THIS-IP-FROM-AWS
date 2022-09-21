e696341f1e3c81aedb33f0822eaacd0c55306624
Daniel Reigada <daniel.reigada@gmail.com>
2022-09-21 17:50:23 +0100

    Styling lol

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import './App.css';
-import {IpChecker} from './components/ip-checker';
+import { IpChecker } from './components/ip-checker';
 
 function App() {
-  return (
+  return (<>
     <div className="App">
       <header className="App-header">
-        <IpChecker/>
+        <h1>IS THIS IP FROM AWS?</h1>
+        {/* <div style={{backgroundColor: "white", marginBottom: "30px"}}>
+          <img width="100px" src='https://upload.wikimedia.org/wikipedia/commons/9/93/Amazon_Web_Services_Logo.svg'></img>
+        </div> */}
       </header>
+      <article className='App-content'>
+        <IpChecker />
+      </article>
+      <footer className="App-footer">
+        <p><a href='https://github.com/DReigada/aws-ip-space'>Source</a></p>
+        <p>|</p>
+        <p>Made by: <a href='https://github.com/DReigada'>dreigada</a></p>
+      </footer>
+
     </div>
-  );
+  </>);
 }
 
 export default App;
--- src/components/ip-checker.tsx
+++ src/components/ip-checker.tsx
@@ -1,5 +1,5 @@
 import React, { ChangeEvent, useEffect, useState } from 'react'; // we need this to make JSX compile
-
+import './ip-checker.css'
 import IPCIDR from 'ip-cidr';
 import * as ip_cidr from 'ip-cidr';
 
@@ -20,11 +20,13 @@ async function fetchData(): Promise<Data> {
 
 export const IpChecker = () => {
   const [data, setData] = useState<Data>({ ipv4Cidrs: [], ipv6Cidrs: [] });
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<Error | null>(null);
   const [isAWS, setIsAWS] = useState(false);
+  
+  const [fetching, setFetching] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
 
   useEffect(() => {
+    setFetching(true)
     fetchData()
       .then(setData)
       .then(_ => setError(null))
@@ -32,17 +34,16 @@ export const IpChecker = () => {
         console.error(e)
         setError(e)
       })
-      .finally(() =>
-        setLoading(false)
+      .finally(() => {
+        setFetching(false)
+      }
       )
   }, []);
 
   const onNameChange = (e: ChangeEvent<HTMLInputElement>) => {
     const ip = e.target.value;
 
-    if (!loading && ip_cidr.isValidAddress(ip)) {
-      console.log("ASDFAS")
-
+    if (!fetching && ip_cidr.isValidAddress(ip)) {
       const result = data.ipv4Cidrs.some(cidr => {
         const result = cidr.contains(ip);
         //console.log(result)
@@ -55,12 +56,14 @@ export const IpChecker = () => {
     }
   }
 
-  return <>
-    <span>
-      <input type="text" placeholder="1.1.1.1" onChange={onNameChange} />
-      {error &&
-        <p style={{ color: "red" }}>{error.message}</p> ||
-        <p>{isAWS ? "Is AWS" : "Is not AWS"}</p>}
-    </span>
-  </>
+  if (fetching) {
+    return <p>fetching ip ranges...</p>
+  } else if (error) {
+    return <p style={{ color: "red" }}>{error.message}</p>
+  } else {
+    return <>
+      <input className='ipInput' type="text" placeholder="IPv4 address" onChange={onNameChange} />
+      <p>{isAWS ? "Yes!" : "No."}</p>
+    </>
+  }
 }
